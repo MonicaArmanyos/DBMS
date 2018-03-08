855df4d13341c009a91d1756304d415f56f80d07
Iman <iman.mansour94@gmail.com>
2018-03-08 16:14:58 +0200

    show_databases function

--- db.sh
+++ db.sh
@@ -8,16 +8,15 @@
 
 show_databases(){
 
-	DBMS_DIR="/home/DBMS"
-	databases=`ls`
+	DBMS_DIR="/home/iman/DBMS"
+	ls $DBMS_DIR > $DBMS_DIR/.databases
 	count=0
+	databases=`awk ' { print $1 } ' $DBMS_DIR/.databases`
+	#echo $databases
 	for db in $databases
 	do
-		test -d $db && echo "((++$count))- $db"
+		test -d $DBMS_DIR/$db && echo "$((++count))- $db"
 	done
-
-
-
 }
 
 
